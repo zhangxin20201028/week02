b4af56ffa334c5e71214f8e6281909f245ee1ea2
zhangxin <2481510581@qq.com>
2020-05-05 18:57:38 +0800

    增加randomutil

--- src/main/java/com/zhangxin/common/utils/RandomUtil.java
+++ src/main/java/com/zhangxin/common/utils/RandomUtil.java
@@ -14,43 +14,58 @@ public class RandomUtil {
 
 	// 方法1：返回min-max之间的随机整数（包含min和max值），例如返回1-3之间的随机数，那么返回1或2或3都是正确的，返回4就不对。 (5分)
 	public static int random(int min, int max) {
-		if(max<=min)
+		if (max <= min)
 			throw new RuntimeException("请输入正确的值");
 		Random r = new Random();
-		return  r.nextInt( max -min +1) + min;   
+		return r.nextInt(max - min + 1) + min;
 	}
 
 	// 方法2：在最小值min与最大值max之间截取subs个不重复的随机数。例如在1-10之间取3个不重复的随机数，那么[2,6,9]是对的，[3,5,5]则不对，因为5重复了。应用场景：在100篇文章中随机取10篇文章，月考可能会使用到。
 	// (8分)
 	public static int[] subRandom(int min, int max, int subs) {
-		
-		int[] result = new int[subs];//结果数组
-		
-		HashSet<Integer> set = new HashSet<Integer>();//用来过滤重复的数据
-	     while(set.size()!=subs) {
-	    	 set.add(random(min,max));
-	     }
-		//遍历set ，把值放入数组
-	     int i=0;
-	     for (Integer value : set) {
-	    	 result[i]=value;
-	    	 i++;
+
+		int[] result = new int[subs];// 结果数组
+
+		HashSet<Integer> set = new HashSet<Integer>();// 用来过滤重复的数据
+		while (set.size() != subs) {
+			set.add(random(min, max));
+		}
+		// 遍历set ，把值放入数组
+		int i = 0;
+		for (Integer value : set) {
+			result[i] = value;
+			i++;
 		}
 		return result;
-		
+
 	}
 
-	// 方法3：返回1个  1-9,a-Z之间的随机字符。 (8分)
+	// 方法3：返回1个 1-9,a-Z之间的随机字符。 (8分)
 	public static char randomCharacter() {
-		String str ="123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
-		return str.charAt(random(0, str.length()-1));
+		String str = "123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
+		return str.charAt(random(0, str.length() - 1));
 	}
 
 	// 方法4：返回参数length个字符串(5分)，方法内部要调用randomCharacter()方法 (4分)
 	public static String randomString(int length) {
 		StringBuffer str = new StringBuffer();
-		for(int i=0 ;i<length ;i ++) {
+		for (int i = 0; i < length; i++) {
 			str.append(randomCharacter());
+		}
+		return str.toString();
+	}
+
+	// 方法5：返回1个 1-9随机数字。 (8分)
+	public static int random() {
+		String str = "1234567890";
+		return str.charAt(random(0, str.length() - 1));
+	}
+
+	// 方法6：返回参数length个数字(5分)， (4分)
+	public static String random(int length) {
+		StringBuffer str = new StringBuffer();
+		for (int i = 0; i < length; i++) {
+			str.append(random());
 		}
 		return str.toString();
 	}
